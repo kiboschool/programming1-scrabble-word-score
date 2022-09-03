8cc4c6c5e6e2b37f912b63fbd12f4dc072b5916a
Rob Cobb <rwcobbjr@gmail.com>
2022-09-02 23:43:28 -0600

    add solution

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-# See the full instructions in the instructions tab
-
 print("Generate the scrabble score for a word.")
-
-# Get a word as input
 word = input("Enter a word: ")
 
-# Start the score at 0
 score = 0
 
-# INSTRUCTIONS:
-# Loop over the letters in the word
-## At each step in the loop, add the value for that letter
-## To find the letter value, check if the letter is in the set of letters for each score, like this:
+for letter in word:
+  if letter in 'aeilnorstu':
+    score += 1
+  elif letter in 'dg':
+    score += 2
+  elif letter in 'bcmp':
+    score += 3
+  elif letter in 'fhvwy':
+    score += 4
+  elif letter in 'k':
+    score += 5
+  elif letter in 'jx':
+    score += 8
+  elif letter in 'qz':
+    score += 10
+  else:
+    score += 0
 
-  # if letter in 'aeilnorstu':
-  #   score += 1
-  # elif letter in 'dg':
-  #   score += 2
-
-## See the full table of letter values in the Instructions.
-
-## Print out the score
 print(f"{word} has a scrabble score of {score}")
