afac28757e280e6333a84c8e3379f43a00333bdb
Rob Cobb <rwcobbjr@gmail.com>
2022-10-15 18:21:04 +0000

    fix module cleanup

--- test.py
+++ test.py
@@ -9,28 +9,37 @@ class Test(TestCase):
     @patch('builtins.input', return_value="apple")
     def test_apple(self, mock_input, mock_print):
         import main
-        mock_print.assert_called_with("apple has a scrabble score of 9")
-        sys.modules.pop('main')
+        try:
+            mock_print.assert_called_with("apple has a scrabble score of 9")
+        finally:
+            sys.modules.pop('main')
 
     @patch('builtins.print')
     @patch('builtins.input', return_value="quixotic")
     def test_quixotic(self, mock_input, mock_print):
         import main
-        mock_print.assert_called_with("quixotic has a scrabble score of 26")
-        sys.modules.pop('main')
+        try:
+            mock_print.assert_called_with("quixotic has a scrabble score of 26")
+        finally:
+            sys.modules.pop('main')
 
     @patch('builtins.print')
     @patch('builtins.input', return_value="freezer")
     def test_freezer(self, mock_input, mock_print):
         import main
-        mock_print.assert_called_with("freezer has a scrabble score of 19")
-        sys.modules.pop('main')
+        try:
+            mock_print.assert_called_with("freezer has a scrabble score of 19")
+        finally:
+            sys.modules.pop('main')
 
     @patch('builtins.print')
     @patch('builtins.input', return_value="abcdefghijklmnopqrstuvwxyz")
     def test_alphabet(self, mock_input, mock_print):
         import main
-        mock_print.assert_called_with("abcdefghijklmnopqrstuvwxyz has a scrabble score of 87")
-        sys.modules.pop('main')
+        try:
+            mock_print.assert_called_with("abcdefghijklmnopqrstuvwxyz has a scrabble score of 87")
+        finally:
+            sys.modules.pop('main')
+
 if __name__ == '__main__':
     unittest.main()
