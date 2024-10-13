d52673ae625edd7d5667c3e26958db1e71b4e605
David Walter <david@kibo.school>
2024-10-13 10:50:46 -0700

    Initial commit (squashed)

---
+++ test_main.py
@@ -0,0 +1,45 @@
+from unittest.mock import patch
+from unittest import TestCase
+import unittest
+import sys
+import io
+
+class Test(TestCase):
+    @patch('builtins.print')
+    @patch('builtins.input', return_value="apple")
+    def test_apple(self, mock_input, mock_print):
+        import main
+        try:
+            mock_print.assert_called_with("apple has a scrabble score of 9")
+        finally:
+            sys.modules.pop('main')
+
+    @patch('builtins.print')
+    @patch('builtins.input', return_value="quixotic")
+    def test_quixotic(self, mock_input, mock_print):
+        import main
+        try:
+            mock_print.assert_called_with("quixotic has a scrabble score of 26")
+        finally:
+            sys.modules.pop('main')
+
+    @patch('builtins.print')
+    @patch('builtins.input', return_value="freezer")
+    def test_freezer(self, mock_input, mock_print):
+        import main
+        try:
+            mock_print.assert_called_with("freezer has a scrabble score of 19")
+        finally:
+            sys.modules.pop('main')
+
+    @patch('builtins.print')
+    @patch('builtins.input', return_value="abcdefghijklmnopqrstuvwxyz")
+    def test_alphabet(self, mock_input, mock_print):
+        import main
+        try:
+            mock_print.assert_called_with("abcdefghijklmnopqrstuvwxyz has a scrabble score of 87")
+        finally:
+            sys.modules.pop('main')
+
+if __name__ == '__main__':
+    unittest.main()
